8646e388e29887621fbea487d78cafb3203effa1
George Sproston <gsproston94@gmail.com>
2024-04-17 23:04:31 +0100

    1 rocket starts orbiting around its closest planet (#5)
    * Starting the rocket above the planet
    
    * Falling towards the planet
    
    * Unstable orbit
    
    * Stable orbit
    
    * clarifying comment

--- Rocket.gd
+++ Rocket.gd
@@ -1,6 +1,16 @@
 extends Area2D
 
 
+const SIZE = 16
+const HORIZONTAL_VELOCITY = 70
+# a few standard orders out, but helps for simplicity
+const GRAVITATIONAL_CONSTANT = 6.674
+
+var acceleration = Vector2.ZERO
+var velocity = Vector2.ZERO
+var local_planet: Area2D = null
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -8,9 +18,27 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if (local_planet == null):
+		return
+		
+	var direction = local_planet.position - position
+	acceleration = direction.normalized() * GRAVITATIONAL_CONSTANT * (local_planet.mass / direction.length_squared())
+	velocity += acceleration * delta
+	position += velocity * delta
 
 
 func _draw():
-	draw_rect(Rect2(0, 0, 16, 16), Color.LIGHT_GREEN)
+	draw_rect(Rect2(-SIZE/2, -SIZE/2, SIZE, SIZE), Color.LIGHT_GREEN)
+	draw_rect(Rect2(-1, -1, 2, 2), Color.RED)
+	
+	
+func set_local_planet(planet: Area2D):
+	local_planet = planet
+	# set the rocket's position
+	position = local_planet.position - Vector2(0, local_planet.radius_px * 1.5)
+	
+	# give the rocket some horizontal motion to get it falling
+	var direction = local_planet.position - position
+	velocity = direction.normalized().orthogonal() * HORIZONTAL_VELOCITY
+	
 
--- main.gd
+++ main.gd
@@ -1,18 +1,12 @@
-extends Area2D
-
-@export var radius_px = 100
-@export var mass = 100000
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$CollisionShape2D.shape.radius = radius_px
+	# Start the rocket above the planet
+	$Rocket.set_local_planet($Planet)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _draw():
-	draw_circle(Vector2(0,0), radius_px, Color.CORAL)
