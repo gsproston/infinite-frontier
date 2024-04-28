8a506d319103d1386571bda66113003d088ed93a
gsproston <gsproston94@gmail.com>
2024-04-28 11:24:51 +0100

    Orbits are behaving better now

--- Rocket.gd
+++ Rocket.gd
@@ -19,8 +19,8 @@ func _process(delta):
 	if (local_planet == null):
 		return
 		
-	var direction = local_planet.position - position
-	acceleration = direction.normalized() * GRAVITATIONAL_CONSTANT * (local_planet.mass / direction.length_squared())
+	var direction_to_planet = local_planet.position - position
+	acceleration = direction_to_planet.normalized() * GRAVITATIONAL_CONSTANT * (local_planet.mass / direction_to_planet.length_squared())
 	velocity += acceleration * delta
 	position += velocity * delta
 	
@@ -34,19 +34,19 @@ func _process(delta):
 	
 func draw_orbit():
 	if (local_planet):
-		var direction = local_planet.position - position
+		var direction_from_planet = position - local_planet.position
 		var gravitational_parameter = GRAVITATIONAL_CONSTANT * local_planet.mass
-		var specific_angular_momentum = direction.cross(velocity)
+		var specific_angular_momentum = direction_from_planet.cross(velocity)
 		var semilatus_rectum = pow(specific_angular_momentum, 2) / gravitational_parameter
 		
-		var eccentricity_vector = (pow(velocity.length(), 2) / gravitational_parameter - 1 / direction.length()) * direction
-		eccentricity_vector = eccentricity_vector - (direction.dot(velocity) / gravitational_parameter) * velocity
+		var eccentricity_vector = (pow(velocity.length(), 2) / gravitational_parameter - 1 / direction_from_planet.length()) * direction_from_planet
+		eccentricity_vector = eccentricity_vector - (direction_from_planet.dot(velocity) / gravitational_parameter) * velocity
 		var orbital_eccentricity = eccentricity_vector.length()
 		
 		var distance_to_planet = position.distance_to(local_planet.position)
 		var expected_angle = acos((semilatus_rectum / distance_to_planet - 1) / orbital_eccentricity)
-		var actual_angle = direction.angle()
-		var angle_diff = actual_angle - expected_angle
+		var actual_angle = direction_from_planet.angle()
+		var angle_diff = expected_angle - abs(actual_angle)
 		
 		var num_points = 128
 		var points = PackedVector2Array()
@@ -54,7 +54,7 @@ func draw_orbit():
 		for n in num_points:
 			var theta = (float(n) / num_points) * TAU
 			var r = semilatus_rectum / (1 + orbital_eccentricity * cos(theta))
-			points.append(r * Vector2.from_angle(theta + angle_diff) + direction)
+			points.append(r * Vector2.from_angle(theta + angle_diff) - direction_from_planet)
 		# close the loop
 		points.append(points[0])
 	
@@ -82,10 +82,11 @@ func _draw():
 func set_local_planet(planet: Area2D):
 	local_planet = planet
 	# set the rocket's position
-	position = local_planet.position - Vector2(local_planet.radius_px * 1.5, 0)
+	position = local_planet.position + Vector2(local_planet.radius_px * 1.5, 0)
 	
 	# give the rocket some horizontal motion to get it falling
-	var direction = local_planet.position - position
-	velocity = direction.normalized().rotated(PI /3) * 70
+	# TODO calculate this
+	var direction_to_planet = local_planet.position - position
+	velocity = direction_to_planet.normalized().orthogonal() * 70
 	
 
