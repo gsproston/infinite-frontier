cd0d040a462162eefe846784e855e8c7225baa6a
George Sproston <gsproston94@gmail.com>
2024-04-28 12:10:58 +0100

    10 fix orbits (#13)
    * Fix starting orbit
    
    * Can now accelerate and decelerate
    
    * Getting orbital eccentricity from eccentricity vector
    
    * WIP: calculating angle diff
    
    * Orbits are behaving better now
    
    * Better calculation of the expected angle
    
    * Working angled orbits

--- Rocket.gd
+++ Rocket.gd
@@ -2,14 +2,12 @@ extends Area2D
 
 
 const SIZE = 8
-const HORIZONTAL_VELOCITY = 80
 # a few standard orders out, but helps for simplicity
 const GRAVITATIONAL_CONSTANT = 6.674
 
 var acceleration = Vector2.ZERO
 var velocity = Vector2.ZERO
 var local_planet: Area2D = null
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,22 +19,41 @@ func _process(delta):
 	if (local_planet == null):
 		return
 		
-	var direction = local_planet.position - position
-	acceleration = direction.normalized() * GRAVITATIONAL_CONSTANT * (local_planet.mass / direction.length_squared())
+	var direction_to_planet = local_planet.position - position
+	acceleration = direction_to_planet.normalized() * GRAVITATIONAL_CONSTANT * (local_planet.mass / direction_to_planet.length_squared())
 	velocity += acceleration * delta
 	position += velocity * delta
+	
+	if Input.is_action_pressed("accelerate"):
+		velocity += velocity.normalized() * delta * 10
+	elif Input.is_action_pressed("decelerate"):
+		velocity -= velocity.normalized() * delta * 10
 	
 	queue_redraw()
 	
 	
 func draw_orbit():
 	if (local_planet):
-		var direction = local_planet.position - position
+		var direction_from_planet = position - local_planet.position
 		var gravitational_parameter = GRAVITATIONAL_CONSTANT * local_planet.mass
-		var specific_angular_momentum = direction.cross(velocity)
+		var specific_angular_momentum = direction_from_planet.cross(velocity)
 		var semilatus_rectum = pow(specific_angular_momentum, 2) / gravitational_parameter
-		var specific_orbital_energy = velocity.length_squared() / 2 - gravitational_parameter / position.distance_to(local_planet.position)
-		var orbital_eccentricity = sqrt(1 + (2 * specific_orbital_energy * pow(specific_angular_momentum, 2)) / pow(gravitational_parameter, 2))
+		
+		var eccentricity_vector = (pow(velocity.length(), 2) / gravitational_parameter - 1 / direction_from_planet.length()) * direction_from_planet
+		eccentricity_vector = eccentricity_vector - (direction_from_planet.dot(velocity) / gravitational_parameter) * velocity
+		var orbital_eccentricity = eccentricity_vector.length()
+		
+		var distance_to_planet = position.distance_to(local_planet.position)
+		# expected angle could be one of two values
+		var expected_angle = acos((semilatus_rectum / distance_to_planet - 1) / orbital_eccentricity)
+		if (velocity.dot(acceleration) < 0):
+			# decelerating
+			expected_angle = -abs(expected_angle)
+		else:
+			# accelerating
+			expected_angle = abs(expected_angle)
+		var actual_angle = direction_from_planet.angle()
+		var angle_diff = expected_angle - actual_angle
 		
 		var num_points = 128
 		var points = PackedVector2Array()
@@ -44,7 +61,7 @@ func draw_orbit():
 		for n in num_points:
 			var theta = (float(n) / num_points) * TAU
 			var r = semilatus_rectum / (1 + orbital_eccentricity * cos(theta))
-			points.append(r * Vector2.from_angle(theta) + direction)
+			points.append(r * Vector2.from_angle(theta - angle_diff) - direction_from_planet)
 		# close the loop
 		points.append(points[0])
 	
@@ -72,10 +89,11 @@ func _draw():
 func set_local_planet(planet: Area2D):
 	local_planet = planet
 	# set the rocket's position
-	position = local_planet.position - Vector2(local_planet.radius_px * -1.5, 0)
+	position = local_planet.position + Vector2(local_planet.radius_px * 2.5, 0)
 	
 	# give the rocket some horizontal motion to get it falling
-	var direction = local_planet.position - position
-	velocity = direction.normalized().orthogonal() * HORIZONTAL_VELOCITY
+	# TODO calculate this
+	var direction_to_planet = local_planet.position - position
+	velocity = direction_to_planet.normalized().rotated(PI/3) * 50
 	
 
