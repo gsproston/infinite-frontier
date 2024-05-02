8be61ed51781b8860310f4efdfc90b760959b06e
gsproston <gsproston94@gmail.com>
2024-05-02 21:42:24 +0100

    Less duplicated code

--- scenes/main.gd
+++ scenes/main.gd
@@ -4,7 +4,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Start the rocket above the planet
-	$Rocket.set_local_planet($Planet)
+	reset_rocket()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,4 +14,9 @@ func _process(delta):
 
 func _on_rocket_area_entered(area):
 	# Reset the rocket if it crashes
+	reset_rocket()
+
+
+
+func reset_rocket():
 	$Rocket.set_local_planet($Planet)
